2fb1f7c6c37f3c117e1217b37cb7b215a8487794
velan-3 <106436639+velan-3@users.noreply.github.com>
2023-07-26 10:46:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from flask import Flask, jsonify
-import os
+import numpy as np
+from flask import Flask,jsonify
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
+#
+#@app.route('/predict_api',methods=['POST'])
+#def predict_api():
+   # '''
+    #For direct API calls trought request
+    #'''
+    #data = request.get_json(force=True)
+    #prediction = model.predict([np.array(list(data.values()))])
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    #output = prediction[0]
+    #return jsonify(output)
+@app.route('/predict_api/<int:feature1>/<int:feature2>/<int:feature3>/<int:feature4>', methods=['GET'])
+def predict_api(feature1, feature2, feature3, feature4):
+    # Convert the input features to a numpy array
+    input_data = np.array([[feature1, feature2, feature3, feature4]])
 
+    # Make the prediction using the model
+    prediction = model.predict(input_data)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
